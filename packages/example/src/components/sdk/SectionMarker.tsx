eebe31200aaff66a8961b356b9d8fd5981af73b4
Benedict Allerberger <me@bene.dev>
2024-03-31 18:32:24 +0200

    wip

--- packages/example/src/components/sdk/SectionMarker.tsx
+++ packages/example/src/components/sdk/SectionMarker.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+
 type SectionMarkerProps = {
   sectionId: string;
 };
 
 export function SectionMarker({ sectionId }: SectionMarkerProps) {
-  // Go get the section get the element before this in the DOM
-  return <div className="hidden" data-inhalt-marker-for={sectionId} />;
+  const elRef = useRef<HTMLDivElement>(null);
+
+  // Always up to date rect of the section
+  // Send via postMessage to the parent window (editor)
+  // Editor can use this to calculate position of tools (insert button etc.)
+  const [rect, setRect] = useState<DOMRect | null>(null);
+
+  useEffect(() => {
+    if (!elRef.current) {
+      return;
+    }
+
+    const section = elRef.current.parentElement?.nextElementSibling;
+    if (!section) {
+      return;
+    }
+
+    const onChange = () => {
+      setRect(section.getBoundingClientRect());
+    };
+
+    const resizeObserver = new ResizeObserver(onChange);
+    resizeObserver.observe(section);
+
+    document.addEventListener("scroll", onChange);
+
+    return () => {
+      resizeObserver.disconnect();
+      document.removeEventListener("scroll", onChange);
+    };
+  }, []);
+
+  return (
+    <div ref={elRef} className="hidden" data-inhalt-marker-for={sectionId} />
+  );
 }
