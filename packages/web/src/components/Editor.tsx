c2ddd298b3470c81b218b037b1351582b9ece6a6
Benedict Allerberger <me@bene.dev>
2024-04-01 22:50:47 +0200

    Add `PropsEditorPanel`

--- packages/web/src/components/Editor.tsx
+++ packages/web/src/components/Editor.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
-import { AddSectionTool } from "../components/AddSectionTool";
+
 import { useSections } from "../hooks/useSectionts";
+import { AddSectionTool } from "./AddSectionTool";
+import PropsEditorPanel from "./PropsEditorPanel";
 
 const page = {
   id: "45d05b20-8651-42e5-b41d-3b7cd533d73c",
@@ -13,15 +15,31 @@ export function Editor() {
   // State
   const [insertIndex, setInsertIndex] = useState(0);
   const [isToolOpen, setIsToolOpen] = useState(false);
+  const [isPropsEditorOpen, setIsPropsEditorOpen] = useState(false);
+  const [hoveredSectionId, setHoveredSectionId] = useState<string | null>(null);
 
   // DOM refs
   const iframeEl = useRef<HTMLIFrameElement>(null);
   const indicatorEl = useRef<HTMLDivElement>(null);
   const addSectionToolEl = useRef<HTMLDivElement>(null);
 
+  const resetTools = () => {
+    setIsToolOpen(false);
+    setHoveredSectionId(null);
+    indicatorEl.current?.classList.add("hidden");
+    addSectionToolEl.current?.classList.add("hidden");
+  };
+
+  // Hide tools when props editor or toolbar is closed
+  useEffect(() => {
+    if (!isPropsEditorOpen && !isToolOpen) {
+      resetTools();
+    }
+  }, [isToolOpen, isPropsEditorOpen]);
+
   useEffect(() => {
     const onMove = (e: MouseEvent) => {
-      if (!iframeEl.current || isToolOpen) {
+      if (!iframeEl.current || isToolOpen || isPropsEditorOpen) {
         return;
       }
 
@@ -47,15 +65,17 @@ export function Editor() {
 
         if (y < rect.top + 20) {
           addSectionToolEl.current?.classList.remove("hidden");
-          addSectionToolEl.current!.style.top = `${rect.top}px`;
+          addSectionToolEl.current!.style.top = `${rect.top - 0.5}px`;
           setInsertIndex(order);
         } else if (y > rect.top + rect.height - 20) {
           addSectionToolEl.current?.classList.remove("hidden");
-          addSectionToolEl.current!.style.top = `${rect.top + rect.height}px`;
+          addSectionToolEl.current!.style.top = `${rect.top + rect.height + 0.5}px`;
           setInsertIndex(order + 1);
         } else {
           addSectionToolEl.current!.classList.add("hidden");
         }
+
+        setHoveredSectionId(sectionId);
       } else {
         indicatorEl.current!.classList.add("hidden");
       }
@@ -63,35 +83,39 @@ export function Editor() {
 
     document.addEventListener("mousemove", onMove);
     return () => document.removeEventListener("mousemove", onMove);
-  }, [isToolOpen, sections]);
-
-  useEffect(() => {
-    if (!isToolOpen) {
-      indicatorEl.current?.classList.add("hidden");
-      addSectionToolEl.current?.classList.add("hidden");
-    }
-  }, [isToolOpen]);
+  }, [isPropsEditorOpen, isToolOpen, sections]);
 
   return (
-    <div className="w-[100dvw] h-[100dvh] flex flex-col bg-gray-50">
-      <div className="h-12 flex p-4 items-center justify-between">
-        <p className="font-bold text-lg">Home</p>
-        <button className="bg-black rounded-lg shadow px-3 py-1 text-white">
-          Save
-        </button>
-      </div>
+    <>
+      {hoveredSectionId && (
+        <PropsEditorPanel
+          pageId={page.id}
+          sectionId={hoveredSectionId}
+          isOpen={isPropsEditorOpen}
+          setIsOpen={setIsPropsEditorOpen}
+        />
+      )}
 
-      <div className="flex-1 px-4 pb-4">
-        <div className="relative overflow-hidden rounded-xl shadow-xl w-full h-full bg-white">
-          <div
-            ref={indicatorEl}
-            className="absolute bg-gray-200 rounded opacity-25"
-          />
-          <div className="absolute inset-0 z-10 bg-transparent flex">
-            <div className="flex-1 relative">
+      <div className="w-[100dvw] h-[100dvh] flex flex-col bg-gray-50">
+        <div className="h-12 flex p-4 items-center justify-between">
+          <p className="font-bold text-lg">Home</p>
+          <button className="bg-black rounded-lg shadow px-3 py-1 text-white">
+            Save
+          </button>
+        </div>
+
+        <div className="flex-1 px-4 pb-4">
+          <div className="relative overflow-hidden rounded-xl shadow-xl w-full h-full bg-white">
+            <div className="absolute inset-0 z-10 bg-transparent flex">
+              <div
+                ref={indicatorEl}
+                className="absolute z-10 border-2 border-dashed border-pink-800 rounded-xl bg-opacity-10 bg-pink-800 hover:cursor-pointer"
+                onClick={() => setIsPropsEditorOpen(true)}
+              />
+
               <div
                 ref={addSectionToolEl}
-                className="absolute inset-x-0 w-screen h-0"
+                className="absolute inset-x-0 w-screen h-0 z-20"
               >
                 <AddSectionTool
                   isOpen={isToolOpen}
@@ -102,16 +126,17 @@ export function Editor() {
                 />
               </div>
             </div>
-          </div>
-          <div className="absolute inset-0">
-            <iframe
-              ref={iframeEl}
-              src={`http://localhost:4321/${page.slug === "index" ? "" : page.slug}`}
-              className="w-full h-full"
-            />
+
+            <div className="absolute inset-0">
+              <iframe
+                ref={iframeEl}
+                src={`http://localhost:4321/${page.slug === "index" ? "" : page.slug}`}
+                className="w-full h-full"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
