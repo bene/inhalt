14933b2b511816f9e67f00ba05d0e15e32eef121
Benedict Allerberger <me@bene.dev>
2024-04-02 17:04:16 +0200

    wip

--- packages/api/src/github/build.ts
+++ packages/api/src/github/build.ts
@@ -16,6 +16,7 @@ export async function triggerCloudBuild(cloneUrl: string) {
       },
       {
         name: "oven/bun:1",
+        entrypoint: "bun",
         args: ["install", "--frozen-lock-file"],
       },
       {
