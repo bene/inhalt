aee6b08e27327bbed32ed162d22b94cf10f5b8d6
Benedict Allerberger <me@bene.dev>
2024-04-11 01:41:54 +0200

    Revise config

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -3,16 +3,18 @@ import { z } from "zod";
 const configValidator = z
   .object({
     PUBLIC_URL: z.string().url(),
-    GCP_ARTIFACT_REGISTRY_URL: z.string(),
+    GCP_ARTIFACT_REGISTRY_REGION: z.string(),
+    GCP_ARTIFACT_REGISTRY_REPO: z.string(),
   })
   .transform((config) => ({
     publicUrl: new URL(config.PUBLIC_URL),
-    gcpArtifactRegistryUrl: config.GCP_ARTIFACT_REGISTRY_URL,
+    gcpArtifactRegistryUrl: `${config.GCP_ARTIFACT_REGISTRY_REGION}-docker.pkg.dev/$PROJECT_ID/${config.GCP_ARTIFACT_REGISTRY_REPO}/`,
   }));
 
 export const config = Object.freeze(
   configValidator.parse({
     PUBLIC_URL: import.meta.env.PUBLIC_URL,
-    GCP_ARTIFACT_REGISTRY_URL: import.meta.env.GCP_ARTIFACT_REGISTRY_URL,
+    GCP_ARTIFACT_REGISTRY_REGION: import.meta.env.GCP_ARTIFACT_REGISTRY_REGION,
+    GCP_ARTIFACT_REGISTRY_REPO: import.meta.env.GCP_ARTIFACT_REGISTRY_REPO,
   })
 );
